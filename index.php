a5849dc0dac6c4b406727fcb8a372719539b5294
Steven Rivera <stevenlrr@gmail.com>
2022-07-02 23:16:58 -0400

    API more test added

--- index.php
+++ index.php
@@ -2,38 +2,39 @@
 require './src/Core/core.php';
 
 switch ($http->get_uri()) {
-    case $http->flexible_uri("/api/v1/login") :
+    case $http->flexible_uri("/api/v1/logins") :
         $api->no_auth_needed();
-        $api->set_uri("/api/v1/login");
+        $api->set_uri("/api/v1/logins");
 
-
+            // '/api/v1/logins/password'
+            $response = false;
             $api->add_allow_routes('POST', 'password', 'internal_password');
-            $response['password']['POST'] = false;
             if($api->get_current_route() == 'password') {
                 if(!empty($_POST['email']) && !empty($_POST['password']) && $user->login_with_password($_POST['email'], $_POST['password'])) {
-                    $response['password']['POST'] = $jwt->encode('by_password', $user->email, $user->password_hash);
+                    $response = $jwt->encode('by_password', $user->email, $user->password_hash);
                 }
             }
-            $api->set_responses('POST', 'password', $response['password']['POST'], $response['password']['POST'], 'Failed to log in');
+            $api->set_responses('POST', 'password', $response, $response, 'Failed to log in');
             
-        $api->run();
-        break;
-    case '/api/v1/login/phone' :
-        $api->no_auth_needed();
-        $api->set_uri("/api/v1/login");
-
-
+            // '/api/v1/logins/phone'
+            $response = false;
             $api->add_allow_routes('POST', 'phone', 'internal_phone');
-            $response['phone']['POST'] = false;
             if($api->get_current_route() == 'phone') {
                 if(!empty($_POST['phone']) && $user->send_text_message_with_code($_POST['phone'])) {
-                    $response['phone']['POST'] = true;
+                    $response = true;
                 }
             }
-            $api->set_responses('POST', 'phone', $response['phone']['POST'], 'Login code was sent', 'Failed to send code');
+            $api->set_responses('POST', 'phone', $response, 'Login code was sent', 'Failed to send code');
 
-
-
+            // '/api/v1/logins/phone_verify'
+            $response = false;
+            $api->add_allow_routes('POST', 'phone_verify', 'internal_phone_verify');
+            if($api->get_current_route() == 'phone_verify') {
+                if(!empty($_POST['phone']) && !empty($_POST['code']) && $user->login_with_phone_code($_POST['phone'], $_POST['code'])) {
+                    $response = $jwt->encode('by_phone_code', $user->phone_number, $user->nonce);
+                }
+            }
+            $api->set_responses('POST', 'phone_verify', $response, $response, 'Failed to log in');
         $api->run();
         break;
     case $http->flexible_uri("/api/v1/db/") :
@@ -43,18 +44,14 @@ switch ($http->get_uri()) {
         break;
     case $http->flexible_uri("/api/v1/") :
         $api->set_uri("/api/v1/");
-
-
+            $response = false;
             $api->add_allow_routes('GET', 'records', 'internal_records');
-            $response['records']['GET'] = false;
             if($api->get_current_route() == 'records') {
                 if($api->get_current_route_value() == 1) {
-                    $response['records']['GET'] = json_encode('{"name":"John", "age":30, "car":null}');
+                    $response = json_encode('{"name":"John", "age":30, "car":null}');
                 }
             }
-            $api->set_responses('GET', 'records', $response['records']['GET']);
-
-
+            $api->set_responses('GET', 'records', $response);
         $api->run();
         break;
     default :
